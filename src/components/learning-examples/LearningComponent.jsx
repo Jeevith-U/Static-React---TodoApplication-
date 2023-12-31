a5251e3c11052afc3ceefbe92aa977e324ab5428
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2023-12-31 22:41:59 +0530

    Committing for the first time

---
+++ src/components/learning-examples/LearningComponent.jsx
@@ -0,0 +1,17 @@
+
+import FirstComponent from './FirstComponent copy';
+import SecondComponent from './SecondComponent' ;
+import NewClassComponent from'./NewClassComponent' ;
+import {FifthComponent} from './FirstComponent copy'
+
+export default function LearningComponent() {
+    return (
+      <div className="LearningComponent">
+     
+       <FirstComponent></FirstComponent>
+       <SecondComponent/>
+       <NewClassComponent></NewClassComponent>
+       <FifthComponent/>
+      </div>
+    );
+  }
---
+++ src/components/learning-examples/NewClassComponent.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react"
+
+export default class NewClassComponent extends Component{
+    render(){
+      return(
+      <div className="NewClassComponent">NewClassComponent</div>
+    )
+   }
+  }
